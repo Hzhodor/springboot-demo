f572ef8620f51e01b83b60c6d75d54cd0c307849
Hzhodor <hzhodor@163.com>
2018-01-08 00:41:32 +0800

    20180108

--- src/main/java/cn/itcast/springbootdemo/domain/User.java
+++ src/main/java/cn/itcast/springbootdemo/domain/User.java
@@ -2,10 +2,12 @@ package cn.itcast.springbootdemo.domain;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
+@SuppressWarnings("serial")
 @Data
-public class User {
+public class User implements Serializable {
 	private int id;
 	private String name;
 	private int age;
